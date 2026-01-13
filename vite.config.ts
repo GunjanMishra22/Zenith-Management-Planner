18656123cc5626aa55b7f84dc5f1da7b74c0a73e
GunjanMishra22 <85674510+GunjanMishra22@users.noreply.github.com>
2026-01-13 12:57:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/YOUR_REPO_NAME/', // Important: Add your repo name here
       server: {
         port: 3000,
         host: '0.0.0.0',
